a7a4d7e4ce0ca8ec778464d57a90ec821c796423
o-ddong <odh0112@naver.com>
2024-01-28 20:14:52 +0900

    Feat: mdn 암복호화 기능 추가

--- applications/base/messages.py
+++ applications/base/messages.py
@@ -2,4 +2,4 @@
 
 """ user validation message """
 user_paramter_validation_message = "required parameters not entered"
-user_mdn_validation_message = "mdn excceds 11 digits"
+user_mdn_validation_message = "mdn does not fit the format"
--- applications/users/serializers.py
+++ applications/users/serializers.py
@@ -5,20 +5,29 @@ from applications.users.models import User
 
 
 class MdnSerializer(serializers.ModelSerializer):
-    mdn = serializers.CharField(max_length=11)
+    mdn = serializers.CharField(max_length=11, required=True)
+    password = serializers.CharField(max_length=128, write_only=True)
 
-    def validate(self, attrs):
-        mdn = attrs.get('mdn')
-        password = attrs.get('password')
+    def validate_password(self, password):
+        if not password:
+            raise serializers.ValidationError(user_paramter_validation_message)
 
-        if not mdn or not password:
+        return password
+
+    def validate_mdn(self, mdn):
+        print("==== start validate mdn ====")
+        if not mdn:
             raise serializers.ValidationError(user_paramter_validation_message)
 
         if len(mdn) != 11:
             raise serializers.ValidationError(user_mdn_validation_message)
 
-        return attrs
+        if any(num.isalpha() for num in mdn):
+            raise serializers.ValidationError(user_mdn_validation_message)
+
+        print("==== end validate mdn ====")
+        return mdn
 
     class Meta:
         model = User
-        fields = ['mdn', ]
+        fields = ['mdn', 'password']
